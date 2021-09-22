31f5d11b7d0bd282614126acb3c65d505cba5415
ebanister <eleanor.banister@stockport.gov.uk>
2021-09-22 12:10:10 +0100

    Typo corrected

--- R/create-plot.R
+++ R/create-plot.R
@@ -15,8 +15,8 @@ scatter_plot =
 df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
-  geom_point(aes(colour = "blue")) +
-  labs(title="Brisbane")
+  geom_point(aes(colour = "red")) +
+  labs(title="Sydeny")
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
