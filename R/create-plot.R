f8f6107b468b67466695660a537793cf4aab18ce
ebanister <eleanor.banister@stockport.gov.uk>
2021-09-23 09:31:00 +0100

    Changed header

--- R/create-plot.R
+++ R/create-plot.R
@@ -16,7 +16,7 @@ df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
   geom_point(aes(colour = "red")) +
-  labs(title="Sydney")
+  labs(title="Sydney Temp & Humidity")
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
